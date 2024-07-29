d2450016c9f6e7f95151868c1bbe239bf17c1d2b
cliu239 <cliu239@uic.edu>
2024-07-29 16:07:26 -0500

    Step 2

--- step_2_b_y_ions.py
+++ step_2_b_y_ions.py
@@ -1,14 +1,12 @@
-from expasy import aminoacid, monoisotopic
+from expasy import aminoacid_mw, monoisotopic
 from re import findall as refindall
+from collections import defaultdict
 
-def molecular_weight(molecule: str) -> float:
-    """
-    the function to calculate the molecular weight of the individual amino acid
 
-    :param molecule: string
-    :return: mass of the molecule
-    """
-    return sum(
-        monoisotopic[atom] * int(num or '1')
-        for atom, num in refindall(r'([A-Z][a-z]*)(\d*)', molecule)
-    )
+def protein_weight(protein: str) -> float:
+    # Sum the molecular weights of amino acids
+    mass = sum(aminoacid_mw[aa] for aa in protein)
+    # Adjust for peptide bonds
+    mass -= (len(protein) - 1) * 18.010565
+    return mass
+
--- time_test.py
+++ time_test.py
@@ -1,73 +1,20 @@
-from expasy import expasy_rules
-import re
-from concurrent.futures import ProcessPoolExecutor
-import time
-
-def peptide_cleavage(rule, sequence):
-    """
-    Method for peptide cleavage with the expasy rule selected with regular expression
-    :param rule:
-    :param sequence:
-    :return:
-    """
-    exp_rule = expasy_rules[rule]
-    cut_end_pos_list = [i.start() for i in re.finditer(exp_rule, sequence)] + [(len(sequence) - 1)]
-    cut_start_pos_list = [0] + [i + 1 for i in cut_end_pos_list]
-    cut_start_pos_list.pop()
-
-    pieces_list = {"start_pos": cut_start_pos_list, "end_pos": cut_end_pos_list}
-
-    result = [sequence[pieces_list["start_pos"][i]:pieces_list["end_pos"][i] + 1]
-              for i in range(0, len(pieces_list["start_pos"]) - 1)]
-    return result
-
-def process_entry(args):
-    """
-    Process a single FASTA entry.
-    :param args: tuple containing entry and rule
-    :return:
-    """
-    entry, rule = args
-    lines = entry.strip().split('\n')
-    if len(lines) < 2 or '|' not in lines[0]:
-        return None, None
-    header = lines[0]
-    sequence = ''.join(lines[1:])
-    uniprot_id = header.split('|')[1]
-    return uniprot_id, peptide_cleavage(rule, sequence)
-
-def cleave_fasta_with_enzyme_parallel(rule, filename):
-    """
-    A function to read the fasta file into a {Uniprot_id: cleaved Sequence} dictionary format
-    and process in parallel.
-    :param filename:
-    :return:
-    """
-    with open(filename, 'r') as file_read:
-        file_content = file_read.read()
-        entries = [entry for entry in file_content.split('>') if entry.strip()]
-
-    with ProcessPoolExecutor() as executor:
-        results = list(executor.map(process_entry, [(entry, rule) for entry in entries]))
-
-    # Convert the results into a dictionary
-    uniprot_dict = {uniprot_id: cleaved_sequences for uniprot_id, cleaved_sequences in results}
-    return uniprot_dict
-
-if __name__ == '__main__':
-    start_time = time.time()
-
-    # Example usage
-    rule = 'trypsin'
-    filename = 'human.fasta'
-    uniprot_sequences = cleave_fasta_with_enzyme_parallel(rule, filename)
-
-    end_time = time.time()
-
-    elapsed_time = end_time - start_time
-    print(f"Elapsed time: {elapsed_time:.2f} seconds")
-
-    # Print results
-    for uniprot_id, sequences in uniprot_sequences.items():
-        print(f"UniProt ID: {uniprot_id}")
-        print(f"Cleaved Sequences: {sequences}\n")
+var = {'A': 89.047679,
+       'R': 174.11167600000002,
+       'N': 132.053493,
+       'D': 133.037509,
+       'C': 121.019751,
+       'E': 147.053159,
+       'Q': 146.069143,
+       'G': 75.032029,
+       'H': 155.069477,
+       'I': 131.094629,
+       'L': 131.094629,
+       'K': 146.105528,
+       'M': 149.051051,
+       'F': 165.078979,
+       'P': 115.063329,
+       'S': 105.04259400000001,
+       'T': 119.058244,
+       'W': 204.089878,
+       'Y': 181.073894,
+       'V': 117.07897899999999}
