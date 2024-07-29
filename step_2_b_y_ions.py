19ad6ae282e755837899fbadfa6cb4ce1207c66d
cliu239 <cliu239@uic.edu>
2024-07-29 16:36:31 -0500

    Step 2 b-y ions

--- step_2_b_y_ions.py
+++ step_2_b_y_ions.py
@@ -10,3 +10,23 @@ def protein_weight(protein: str) -> float:
     mass -= (len(protein) - 1) * 18.010565
     return mass
 
+def cal_b_y_ion_mass(peptide):
+    """
+    Takes the peptide sequence and returns a dataframe of b and y ion masses.
+    :param peptide: The peptide sequence
+    :return: DataFrame with b and y ion masses
+    """
+    peptide = peptide.upper()
+    lst = []
+    # Compute b and y ion masses
+    for i in range(1, len(peptide) + 1):
+        b_ion = peptide[:i]
+        y_ion = peptide[i-1:]
+
+        b_mass = protein_weight(b_ion) + 1.007825 - 18.010565
+        y_mass = protein_weight(y_ion) + 19.01839 - 18.010565  # 19.01839 accounts for the proton and H2O
+
+        lst.append(b_mass)
+        lst.append(y_mass)
+
+    return lst
