5d5a9accb4c9a03761b894599ae2aa9993122105
cliu239 <cliu239@uic.edu>
2024-08-07 11:18:14 -0500

    Step 2

--- step_1_fasta_cleavage.py
+++ step_1_fasta_cleavage.py
@@ -3,22 +3,27 @@ import re
 from multiprocessing import Pool
 
 
-def peptide_cleavage(rule, sequence):
+def peptide_cleavage(rule:str, sequence:str, mis_cleavage:int = 2, min_len:int = 7, max_len:int = 50):
     """
-    Method for peptide cleavage with the expasy rule selected with regular expression
+    Method for peptide cleavage with the expasy rule selected with regular expression. Default min len is 7 and max len
+    is 50 with miscleavage of 2.
     :param rule:
     :param sequence:
     :return:
     """
     exp_rule = expasy_rules[rule]
     cut_end_pos_list = [i.start() for i in re.finditer(exp_rule, sequence)] + [(len(sequence) - 1)]
-    cut_start_pos_list = [0] + [i + 1 for i in cut_end_pos_list]
-    cut_start_pos_list.pop()
+    cut_start_pos_list = [0] + [i + 1 for i in cut_end_pos_list[:-1]]
 
     pieces_list = {"start_pos": cut_start_pos_list, "end_pos": cut_end_pos_list}
 
-    result = [sequence[pieces_list["start_pos"][i]:pieces_list["end_pos"][i] + 1]
-              for i in range(0, len(pieces_list["start_pos"]) - 1)]
+    result = [
+        sequence[cut_start_pos_list[i]:cut_end_pos_list[j]]
+        for i in range(len(cut_start_pos_list))
+        for j in range(i, min(i + mis_cleavage + 1, len(cut_start_pos_list)))
+        if min_len <= len(sequence[cut_start_pos_list[i]:cut_end_pos_list[j]]) <= max_len
+    ]
+
     return result
 
 
@@ -55,6 +60,7 @@ def cleave_fasta_with_enzyme_parallel(rule, filename):
     # Convert the results into a dictionary, filtering out None entries
     uniprot_dict = {uniprot_id: cleaved_sequences for uniprot_id, cleaved_sequences in results if
                     uniprot_id is not None}
+
     return uniprot_dict
 
 
