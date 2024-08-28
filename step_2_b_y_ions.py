fb37f71fa1b465510f7694ef840b1223eed52459
cliu239 <cliu239@uic.edu>
2024-08-28 10:31:40 -0500

    Step 2

--- step_2_b_y_ions.py
+++ step_2_b_y_ions.py
@@ -2,17 +2,32 @@ import math
 import itertools
 import pandas as pd
 from b_y_ion_utility import *
-import pymzml
+from pyteomics import mzml
+import lxml
 
 def read_mzml_file(mzml_file):
-    print("A")
+    with mzml.MzML(mzml_file) as reader:
+        for spectrum in reader:
+            scan_id = spectrum.get('id')
+            ms_level = spectrum.get('ms level')
+            retention_time = spectrum.get('scanList', {}).get('scan', [{}])[0].get('scan start time')
+
+            # Accessing m/z and intensity arrays
+            mz_array = spectrum['m/z array']
+            intensity_array = spectrum['intensity array']
+
+            # Print some information about the spectrum
+            print(f"Spectrum ID: {scan_id}")
+            print(f"MS Level: {ms_level}")
+            print(f"Retention Time: {retention_time} minutes")
+            print(f"Number of peaks: {len(mz_array)}")
 
 
 if __name__ == '__main__':
     import time
 
     start_time = time.time()
-
+    read_mzml_file("OV3-DMSO-n3-F9.mzML")
     end_time = time.time()
 
 
