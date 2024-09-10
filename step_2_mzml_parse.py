a466a3f9cc8b4f3d1341c56c67df259342fc693c
cliu239 <cliu239@uic.edu>
2024-09-10 14:07:07 -0500

    Step 2

--- step_2_mzml_parse.py
+++ step_2_mzml_parse.py
@@ -14,17 +14,15 @@ def read_mzml_file(mzml_file):
     """
     with mzml.MzML(mzml_file) as reader:
         spec_ms1_list = {}
-        spec_ms2_list = {}  # Initialize once outside the loop to store all MS2 spectra
+        spec_ms2_list = {}
 
         for spectrum in reader:
             scan_id = spectrum.get('id')
             ms_level = spectrum.get('ms level')
 
-            # Accessing m/z and intensity arrays
             mz_array = spectrum['m/z array']
             intensity_array = spectrum['intensity array']
 
-            # Extract scan ID (making sure it's formatted correctly)
             scan_id = scan_id.split(' ')[-1].split("=")[1]
 
             # Create a tuple of (m/z, intensity)
@@ -50,11 +48,54 @@ def read_mzml_file(mzml_file):
     return spec_ms1_list, spec_ms2_list
 
 
+def read_mzml_file_nointensity(mzml_file):
+    """
+    This will read the mzML file and return the MS1 and MS2 spectra with only m/z values.
+    :param mzml_file:
+    :return: spec_ms1_list (MS1 spectra with m/z values), spec_ms2_list (MS2 spectra with m/z values)
+    """
+    with mzml.MzML(mzml_file) as reader:
+        spec_ms1_list = {}
+        spec_ms2_list = {}
+
+        for spectrum in reader:
+            scan_id = spectrum.get('id')
+            ms_level = spectrum.get('ms level')
+
+            # Accessing only the m/z array
+            mz_array = spectrum['m/z array']
+
+            # Extract scan ID (making sure it's formatted correctly)
+            scan_id = scan_id.split(' ')[-1].split("=")[1]
+
+            # Only store the m/z array
+            spec = list(mz_array)
+
+            if ms_level == 2:
+                # Access precursor information from the spectrum
+                if 'precursorList' in spectrum:
+                    precursor = spectrum['precursorList']['precursor'][0]
+                    precursor_mz = precursor['selectedIonList']['selectedIon'][0]['selected ion m/z']
+                    precursor_scan_id = precursor['spectrumRef'].split(' ')[-1].split("=")[1]
+
+                    # Add precursor scan id and mz to MS2 spectrum list
+                    if precursor_scan_id not in spec_ms2_list:
+                        spec_ms2_list[precursor_scan_id] = {}
+
+                    spec_ms2_list[precursor_scan_id][precursor_mz] = spec
+
+            elif ms_level == 1:
+                # Store MS1 spectrum with only m/z values
+                spec_ms1_list[scan_id] = spec
+
+    return spec_ms1_list, spec_ms2_list
+
+
 if __name__ == '__main__':
     import time
 
     start_time = time.time()
-    spec1, spec2 = read_mzml_file("OV3-DMSO-n3-F9.mzML")
+    spec1, spec2 = read_mzml_file_nointensity("OV3-DMSO-n3-F9.mzML")
     end_time = time.time()
 
     elapsed_time = end_time - start_time
